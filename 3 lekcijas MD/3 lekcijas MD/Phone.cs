415a9e16073c5c6613c81d619abea8899286849f
DairisMagone <dairis.magone@gmail.com>
2021-10-03 22:45:27 +0300

    1MD 1. uzd

---
+++ 3 lekcijas MD/3 lekcijas MD/Phone.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_lekcijas_MD
+{
+	public class Phone
+	{
+		public string phoneName { get; set; }
+		
+		public string phoneModel { get; set; }
+
+		public int phoneLength { get; set; }
+
+		public int phoneWidth { get; set; }
+
+		public int phoneHeight { get; set; }
+
+		public void Call()
+		{
+
+		}
+		public void SMS()
+		{
+
+		}
+	}
+}
---
+++ 3 lekcijas MD/3 lekcijas MD/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _3_lekcijas_MD
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Phone phone = new Phone();
+
+			Console.WriteLine("What phone brand do You have?");
+			string phoneName = Console.ReadLine();
+
+			phone.phoneName = phoneName;
+
+			Console.WriteLine("What phone Model is it?");
+			string phoneModel = Console.ReadLine();
+
+			phone.phoneModel = phoneModel;
+
+			Console.WriteLine("What is your phone Length?");
+			string textPhoneLength = Console.ReadLine();
+			int phoneLength = int.Parse(textPhoneLength);
+
+			phone.phoneLength = phoneLength;
+
+			Console.WriteLine("What is your phone Width?");
+			string textPhoneWidth = Console.ReadLine();
+			int phoneWidth = int.Parse(textPhoneWidth);
+
+			phone.phoneWidth = phoneWidth;
+
+			Console.WriteLine("What is your phone Height?");
+			string textPhoneHeight = Console.ReadLine();
+			int phoneHeight = int.Parse(textPhoneHeight);
+
+			phone.phoneHeight = phoneHeight;
+
+
+		}
+	}
+}
